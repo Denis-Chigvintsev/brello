e8d58182f37015474608610a435cbad43e72d389
Denis Chigvintsev <chigvin@OrionLLC.ru>
2026-02-13 19:04:21 +0500

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return <div>привет добрый мир</div>;
+  const [x, setX] = useState(0);
+  function handleClick() {
+    setX((prev) => Math.floor(Math.random() * 100));
+  }
+  return (
+    <div>
+      <button onClick={handleClick}>{x}</button>
+    </div>
+  );
 }
 
 export default App;
