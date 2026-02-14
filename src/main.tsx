881148278fd2e0fa4c15a643a4d553f6ca792068
Denis Chigvintsev <chigvin@OrionLLC.ru>
2026-02-15 00:13:51 +0500

    minor

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <MantineProvider defaultColorScheme="auto">
+  <MantineProvider>
     <App />
   </MantineProvider>,
 );
