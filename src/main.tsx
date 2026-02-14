46257c9c3a62cba61f20d881c2d1b56a056a1ae6
Denis Chigvintsev <chigvin@OrionLLC.ru>
2026-02-14 23:23:40 +0500

    minor

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,22 @@
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import { MantineProvider } from '@mantine/core';
+import { createRoot } from "react-dom/client";
+
+import { MantineProvider } from "@mantine/core";
 
 // ...
-import '@mantine/core/styles.css';
-import '@mantine/dates/styles.css';
-import '@mantine/dropzone/styles.css';
-import '@mantine/notifications/styles.css';
-import '@mantine/spotlight/styles.css';
-import '@mantine/tiptap/styles.css';
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+import "@mantine/dropzone/styles.css";
+import "@mantine/notifications/styles.css";
+import "@mantine/spotlight/styles.css";
+import "@mantine/tiptap/styles.css";
+
 // ...
 
-import App from './App.tsx';
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <MantineProvider>
+createRoot(document.getElementById("root")!).render(
+  <MantineProvider defaultColorScheme="auto">
     <App />
   </MantineProvider>,
 );
